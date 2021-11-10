183174af5c0cdfd7c59388f47f957825ac393a68
RBeltran12358 <Ricardo.beltran12358@gmail.com>
2021-11-10 10:58:35 -0800

    Finished Skeleton and "Schedule Analysis" button for AnalysisSettings

--- src/main/java/com/example/poweranalysisproject/AnalysisSettingsController.java
+++ src/main/java/com/example/poweranalysisproject/AnalysisSettingsController.java
@@ -1,4 +1,42 @@
 package com.example.poweranalysisproject;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class AnalysisSettingsController {
+    @FXML
+    private TextField CPUTreshold;
+
+    @FXML
+    private TextField DiskTreshold;
+
+    @FXML
+    private TextField MemoryTreshold;
+
+    @FXML
+    private DatePicker textFieldEndDate;
+
+    @FXML
+    private DatePicker textFieldStartDate;
+
+    @FXML
+    private CheckBox CurrTimeCheckBox;
+
+    @FXML
+    protected void clickButtonScheduleAnalysis() throws IOException {
+        // Before it navigates, do error checking for the Dates, etc...
+        // Set
+        Main.NavigateToNewPage("power-analysis-portal");
+    }
+
+
 }
+
+
+
