b4a316868af38afa974e0886ae61e34c2d017c1d
collinstreun <swissflyer12@comcast.net>
2021-11-17 16:32:22 +0100

    Added data into UserPool

--- src/main/java/com/example/poweranalysisproject/UserPoolController.java
+++ src/main/java/com/example/poweranalysisproject/UserPoolController.java
@@ -1,5 +1,43 @@
 package com.example.poweranalysisproject;
 
-public class UserPoolController extends Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserPoolController extends Controller implements Initializable {
+
+    public TableView<UserProfile> table;
+
+    public TableColumn<UserProfile, String> Name;
+    public TableColumn<UserProfile, String> Building;
+    public TableColumn<UserProfile, String> Room;
+    public TableColumn<UserProfile, String> Department;
+    public TableColumn<UserProfile, String> Selected;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Name.setCellValueFactory(new PropertyValueFactory<>("Name"));
+        Building.setCellValueFactory(new PropertyValueFactory<>("Building"));
+        Room.setCellValueFactory(new PropertyValueFactory<>("Room"));
+        Department.setCellValueFactory(new PropertyValueFactory<>("Department"));
+        Selected.setCellValueFactory(new PropertyValueFactory<>("Selected"));
+        table.setItems(observableList);
+    }
+
+    ObservableList<UserProfile> observableList = FXCollections.observableArrayList(
+               new UserProfile("Collin", "10A", "5", "CS", "Yes"),
+            new UserProfile("Noah", "10A", "5", "CS", "No"),
+            new UserProfile("Sam", "10A", "5", "CS", "Yes"),
+            new UserProfile("Ricardo", "10A", "5", "CS", "No"),
+            new UserProfile("Davide", "10A", "5", "CS", "Yes")
+
+
+
+    );
 }
