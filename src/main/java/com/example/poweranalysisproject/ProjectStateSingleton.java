1ef43dfc005d1362ad4c37e5d6d08554358c7322
RBeltran12358 <Ricardo.beltran12358@gmail.com>
2021-11-26 05:36:06 -0800

    Moved singleton class to the right place

--- src/main/java/com/example/poweranalysisproject/ProjectStateSingleton.java
+++ src/main/java/com/example/poweranalysisproject/ProjectStateSingleton.java
@@ -1,7 +1,9 @@
-import com.example.poweranalysisproject.Report;
+package com.example.poweranalysisproject;
 
 public class ProjectStateSingleton {
     private static final ProjectStateSingleton psSingleton = new ProjectStateSingleton();
+
+    // Ricardos
     private Report currReport;
 
     private ProjectStateSingleton() {}
@@ -9,5 +11,7 @@ public class ProjectStateSingleton {
     public static ProjectStateSingleton getInstance() {
         return psSingleton;
     }
+
+
 
 }
