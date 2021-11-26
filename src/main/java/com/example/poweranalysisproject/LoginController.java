93cd2ea703acf8c40f46b6576666c877a119652b
Noah Yuen <noahyuen19@gmail.com>
2021-11-26 16:32:09 +0100

    starting login verification

--- src/main/java/com/example/poweranalysisproject/LoginController.java
+++ src/main/java/com/example/poweranalysisproject/LoginController.java
@@ -1,12 +1,23 @@
 package com.example.poweranalysisproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class LoginController extends Controller {
+
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField passwordTextField;
+
     @FXML
     protected void goToHomePage() throws IOException {
-        Main.navigateToNewPage("power-analysis-portal");
+        if (usernameTextField.getText().length() == 0 || passwordTextField.getText().length() == 0) {
+            System.out.println("Incorrect username/password combo");
+        } else {
+            Main.navigateToNewPage("power-analysis-portal");
+        }
     }
 }
