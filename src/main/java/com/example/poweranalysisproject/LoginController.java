8cffc283e44d135d188957d357fed8f3e05bed8c
Noah Yuen <noahyuen19@gmail.com>
2021-11-26 16:45:08 +0100

    finishing login check

--- src/main/java/com/example/poweranalysisproject/LoginController.java
+++ src/main/java/com/example/poweranalysisproject/LoginController.java
@@ -1,6 +1,7 @@
 package com.example.poweranalysisproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -15,7 +16,12 @@ public class LoginController extends Controller {
     @FXML
     protected void goToHomePage() throws IOException {
         if (usernameTextField.getText().length() == 0 || passwordTextField.getText().length() == 0) {
-            System.out.println("Incorrect username/password combo");
+            usernameTextField.setStyle("-fx-text-box-border: #FF0000");
+            passwordTextField.setStyle("-fx-text-box-border: #FF0000");
+            Alert fail = new Alert(Alert.AlertType.ERROR);
+            fail.setHeaderText("Log In Failed");
+            fail.setContentText("Please Enter a Valid Username & Password");
+            fail.showAndWait();
         } else {
             Main.navigateToNewPage("power-analysis-portal");
         }
