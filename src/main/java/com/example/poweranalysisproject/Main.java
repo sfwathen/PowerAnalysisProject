726fe7d439411e443d2633a105ff8935aa60e26c
collinstreun <swissflyer12@comcast.net>
2021-11-26 14:41:46 +0100

    Created Singleton

--- src/main/java/com/example/poweranalysisproject/Main.java
+++ src/main/java/com/example/poweranalysisproject/Main.java
@@ -14,6 +14,9 @@ public class Main extends Application {
 
     static final int HEIGHT = 400;
     static final int WIDTH = 600;
+
+    ProjectStateSingleton singleton = ProjectStateSingleton.getInstance();
+
     static Report currReport;
 
     static UserProfile currUser;
--- src/main/java/com/example/poweranalysisproject/ProjectStateSingleton.java
+++ src/main/java/com/example/poweranalysisproject/ProjectStateSingleton.java
@@ -6,10 +6,22 @@ public class ProjectStateSingleton {
     // Ricardos
     private Report currReport;
 
+    private UserProfile currUserProfile;
+
     private ProjectStateSingleton() {}
 
     public static ProjectStateSingleton getInstance() {
         return psSingleton;
+    }
+
+    public void setCurrUserProfile(UserProfile user)
+    {
+        currUserProfile = user;
+    }
+
+    public UserProfile getCurrUserProfile()
+    {
+        return currUserProfile;
     }
 
 
