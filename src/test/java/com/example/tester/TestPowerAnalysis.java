670dbd67f2ab0f9c07bf6886eafe6b3ba9fa0ee3
RBeltran12358 <Ricardo.beltran12358@gmail.com>
2021-11-29 11:38:23 -0800

    Tried to fix code smells in Testing Suite 3.0

--- src/test/java/com/example/tester/TestPowerAnalysis.java
+++ src/test/java/com/example/tester/TestPowerAnalysis.java
@@ -10,14 +10,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestPowerAnalysis {
 
-    final static String building5 = "Building 5";
-    final static String building6 = "Building 6";
-    final static String building7 = "Building 7";
-    final static String matteoName = "Matteo";
-    final static String room3 = "Room 3";
-    final static String room5 = "Room 5";
-    final static String room6 = "Room 6";
-    final static String eeDepartment = "ee";
+    static final String BUILDING5 = "Building 5";
+    static final String BUILDING6 = "Building 6";
+    static final String BUILDING7 = "Building 7";
+    static final String MATTEONAME = "Matteo";
+    static final String ROOM3 = "Room 3";
+    static final String ROOM5 = "Room 5";
+    static final String ROOM6 = "Room 6";
+    static final String EE = "ee";
 
     @Test
     public void loopOneCheckTitleConverter()
@@ -38,49 +38,49 @@ public class TestPowerAnalysis {
     {
         Threshold threshold = new Threshold(11,79, 33);
         Report reportOne = new Report(null, null, null, null,
-                false, threshold, new UserProfile(matteoName, building5, room3
+                false, threshold, new UserProfile(MATTEONAME, BUILDING5, ROOM3
                 , "SW"));
         Report reportTwo = new Report(null, null, null, null,
-                false, threshold, new UserProfile("MatteoOne", building6, room5, "CPE"));
-        Report reportThreeDepartment = new Report(null, null, null, null,
-                false, threshold, new UserProfile("MatteoTwo", building6, room6, eeDepartment));
+                false, threshold, new UserProfile("MatteoOne", BUILDING6, ROOM5, "CPE"));
+        Report reportThrEE = new Report(null, null, null, null,
+                false, threshold, new UserProfile("MatteoTwo", BUILDING6, ROOM6, EE));
 
         assertEquals(4, Report.getId());
         assertNotEquals(reportOne, reportTwo);
-        assertNotEquals(reportTwo, reportThreeDepartment);
-        assertNotEquals(reportOne, reportThreeDepartment);
+        assertNotEquals(reportTwo, reportThrEE);
+        assertNotEquals(reportOne, reportThrEE);
     }
 
     @Test
     public void testUserProfileName()
     {
-        UserProfile user = new UserProfile(matteoName, building5, room3
+        UserProfile user = new UserProfile(MATTEONAME, BUILDING5, ROOM3
                 , "SW");
-        assertEquals(user.getName(), matteoName);
+        assertEquals(user.getName(), MATTEONAME);
     }
 
     @Test
     public void testUserProfileBuilding()
     {
-        UserProfile user = new UserProfile("Davide", building6, "Room 9", eeDepartment);
-        user.setBuilding(building7);
-        assertEquals(user.getBuilding(), building7);
+        UserProfile user = new UserProfile("Davide", BUILDING6, "Room 9", EE);
+        user.setBuilding(BUILDING7);
+        assertEquals(user.getBuilding(), BUILDING7);
     }
 
     @Test
     public void testUserProfileRoom()
     {
-        UserProfile user = new UserProfile(matteoName, building7, room6, "CS");
-        assertEquals(user.getRoom(), room6);
+        UserProfile user = new UserProfile(MATTEONAME, BUILDING7, ROOM6, "CS");
+        assertEquals(user.getRoom(), ROOM6);
     }
 
     @Test
     public void testUserProfileDepartment()
     {
-        UserProfile user = new UserProfile("Davide1", building5, room3
-                , eeDepartment);
-        user.setDepartment(eeDepartment);
-        assertEquals(user.getDepartment(), eeDepartment);
+        UserProfile user = new UserProfile("Davide1", BUILDING5, ROOM3
+                , EE);
+        user.setDepartment(EE);
+        assertEquals(user.getDepartment(), EE);
     }
 
     @Test
