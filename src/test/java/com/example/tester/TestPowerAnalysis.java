a680802fa51acbc4d3a01420b09366719ffb3808
collinstreun <swissflyer12@comcast.net>
2021-11-29 19:50:53 +0100

    Added TestReportID

--- src/test/java/com/example/tester/TestPowerAnalysis.java
+++ src/test/java/com/example/tester/TestPowerAnalysis.java
@@ -26,18 +26,19 @@ public class TestPowerAnalysis {
         assertEquals("This Test Verifies The Usage Of Our Title Converter", pageName);
     }
 
-//    @Test
-//    public void testIdCounter()
-//    {
-//        Report report = new Report(null, null, null, null,
-//                false, null, null);
-//        Report reportOne = new Report(null, null, null, null,
-//                false, null, null);
-//        Report reportTwo = new Report(null, null, null, null,
-//                false, null, null);
-//        assertEquals(Report.getId(), 3);
-//
-//    }
+    @Test
+    public void testIdCounter()
+    {
+        Threshold threshold = new Threshold(11,79, 33);
+        Report reportOne = new Report(null, null, null, null,
+                false, threshold, new UserProfile("Matteo", "Building 5", "Room 3", "SW"));
+        Report reportTwo = new Report(null, null, null, null,
+                false, threshold, new UserProfile("MatteoOne", "Building 6", "Room 5", "CPE"));
+        Report reportThree = new Report(null, null, null, null,
+                false, threshold, new UserProfile("MatteoTwo", "Building 6", "Room 6", "EE"));
+
+        assertEquals(4, Report.getId());
+    }
 
     @Test
     public void testUserProfileName()
@@ -49,45 +50,45 @@ public class TestPowerAnalysis {
     @Test
     public void testUserProfileBuilding()
     {
-        UserProfile user = new UserProfile("Matteo", "Building 5", "Room 3", "SW");
-        user.setBuilding("Building 6");
-        assertEquals(user.getBuilding(), "Building 6");
+        UserProfile user = new UserProfile("Davide", "Building 6", "Room 9", "CPE");
+        user.setBuilding("Building 7");
+        assertEquals(user.getBuilding(), "Building 7");
     }
 
     @Test
     public void testUserProfileRoom()
     {
-        UserProfile user = new UserProfile("Matteo", "Building 5", "Room 3", "SW");
-        assertEquals(user.getRoom(), "Room 3");
+        UserProfile user = new UserProfile("Matteo1", "Building 7", "Room 6", "CS");
+        assertEquals(user.getRoom(), "Room 6");
     }
 
     @Test
     public void testUserProfileDepartment()
     {
-        UserProfile user = new UserProfile("Matteo", "Building 5", "Room 3", "SW");
-        user.setDepartment("CS");
-        assertEquals(user.getDepartment(), "CS");
+        UserProfile user = new UserProfile("Davide1", "Building 5", "Room 3", "EE");
+        user.setDepartment("EE");
+        assertEquals(user.getDepartment(), "EE");
     }
 
     @Test
     public void testThresholdCpu()
     {
-        Threshold threshold = new Threshold(5,40, 45);
+        Threshold threshold = new Threshold(5,41, 46);
         assertEquals(threshold.getCpuThreshold(), 5);
     }
 
     @Test
     public void testThresholdDisk()
     {
-        Threshold threshold = new Threshold(5,40, 45);
-        assertEquals(threshold.getDiskThreshold(), 40);
+        Threshold threshold = new Threshold(2,42, 48);
+        assertEquals(threshold.getDiskThreshold(), 42);
     }
 
     @Test
     public void testThresholdMem()
     {
-        Threshold threshold = new Threshold(5,40, 45);
-        assertEquals(threshold.getMemThreshold(), 45);
+        Threshold threshold = new Threshold(9,45, 48);
+        assertEquals(threshold.getMemThreshold(), 48);
     }
 
 
